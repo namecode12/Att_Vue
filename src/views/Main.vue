b60024002182e8a585c0859351f9c4999698fd78
namecode12 <50198120+namecode12@users.noreply.github.com>
2019-11-25 20:35:56 +0900

    main

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,14 @@
 <template>
 <div>
     <div class="navigation-bar">
-        <img class="logo" src="logo.png">
-        <div @click="menu()">메뉴 소개</div>
-        <div @click="translator()">순우리말 번역</div>
-        <div @click="game()">우리말 게임</div>
+        <img class="logo" src="./logo.svg">
+        
+            <div @click="menu()">메뉴 소개</div>
+            <div @click="translator()">순우리말 번역</div>
+            <div @click="game()">우리말 게임</div>
+        
     </div>
-    <div class="topline"><span>우리말을 지키다<br>우리말의 기본,앝</span></div>
+    <div class="topline"><span class="slogan">우리말을 지키다<br>우리말의 기본,앝</span></div>
     <div class="bottomline"></div>
 </div>
 </template>
@@ -18,28 +20,55 @@ export default {
 </script>
 
 <style>
-body{
-    margin: 0;
+@font-face {
+    font-family: 'NanumGothic';
+    src: url('./NanumGothic.ttf') format('truetype');
 }
+    .slogan{
+        align-items: center;
+        display: flex;
+        font-size: 24px;
+    }
+    body    {
+        margin: 0;
+        height: 100%;
+        font-family: NanumGothic;
+        
+    }
+    .topline{
+        margin-left: 87px;
+        height: 300px;
+        display: flex;
+    }
+    .bottomline{
+        height: 500px;
+        background-color: #2A2A4C
+    }
     .navigation-bar {
     
-    width: 100%;  /* i'm assuming full width */
+    margin-left: 60.8px;
+    margin-right: 60.8px;
+    display: flex;
+    align-items: center;
+    width: 766.4px;  /* i'm assuming full width */
     height: 80px; /* change it to desired width */
 
 }
 .logo {
     display: inline-block;
     vertical-align: top;
-    width: 50px;
-    height: 50px;
-    margin-right: 20px;
-    margin-top: 15px;    /* if you want it vertically middle of the navbar. */
+    width: 98px;
+    height: 24px;
+    margin-left: 30px;
+    margin-right: 10px;
+        /* if you want it vertically middle of the navbar. */
 }
 .navigation-bar > div {
     display: inline-block;
     vertical-align: top;
-    margin-right: 20px;
+    margin-left: 60px;
     height: 80px;        /* if you want it to take the full height of the bar */
     line-height: 80px;    /* if you want it vertically middle of the navbar */
+    font-size: 14px;
 }
 </style>
